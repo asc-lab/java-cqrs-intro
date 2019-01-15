45f4400c47aeb1b801804d09815de10f80524936
Robert Witkowski <robert.witkowski@altkom.pl>
2019-01-15 07:57:01 +0100

    Add project from Spring Initializr with Lombok, JPA and H2.

---
+++ nocqrs/src/main/java/pl/altkom/asc/lab/cqrs/intro/nocqrs/NocqrsApplication.java
@@ -0,0 +1,14 @@
+package pl.altkom.asc.lab.cqrs.intro.nocqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NocqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NocqrsApplication.class, args);
+	}
+
+}
+
---
+++ nocqrs/src/test/java/pl/altkom/asc/lab/cqrs/intro/nocqrs/NocqrsApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.altkom.asc.lab.cqrs.intro.nocqrs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NocqrsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
