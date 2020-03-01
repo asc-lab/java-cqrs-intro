134488fdb873fe0f4c5b04bd10b9b23b65af4ce8
Wojciech Zych <wojciech.zych@asseco.pl>
2020-03-01 15:55:40 +0100

    Modify query builder implementation change to parameters binding query

--- separatemodels/src/main/java/pl/altkom/asc/lab/cqrs/intro/separatemodels/readmodel/PolicyFilterQueryBuilder.java
+++ separatemodels/src/main/java/pl/altkom/asc/lab/cqrs/intro/separatemodels/readmodel/PolicyFilterQueryBuilder.java
@@ -1,13 +1,17 @@
 package pl.altkom.asc.lab.cqrs.intro.separatemodels.readmodel;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.util.StringUtils;
 
 import java.time.LocalDate;
 import java.util.List;
 
 class PolicyFilterQueryBuilder {
+    private static final String PERCENTAGE_SYMBOL = "%";
     private String query = "SELECT policy_id, policy_number, cover_from, cover_to, vehicle, policy_holder, total_premium_amount FROM policy_info_dto";
+    private MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
 
     PolicyFilterQueryBuilder where() {
         query += " WHERE 1 = 1";
@@ -15,41 +19,55 @@ class PolicyFilterQueryBuilder {
     }
 
     PolicyFilterQueryBuilder policyNumberEquals(String policyNumber) {
-        if (!StringUtils.isEmpty(policyNumber)) {
-            query += " AND policy_number = '" + policyNumber + "'";
+        if (isNotEmpty(policyNumber)) {
+            mapSqlParameterSource.addValue("policyNumber", policyNumber);
+            query += " AND policy_number = :policyNumber";
         }
         return this;
     }
 
     PolicyFilterQueryBuilder policyHolderLike(String policyHolder) {
-        if (!StringUtils.isEmpty(policyHolder)) {
-            query += " AND policy_holder like '%" + policyHolder + "%'";
+        if (isNotEmpty(policyHolder)) {
+            mapSqlParameterSource.addValue("policyHolder", getValueForLike(policyHolder));
+            query += " AND policy_holder like :policyHolder";
         }
         return this;
     }
 
-    PolicyFilterQueryBuilder policyStartDateFrom(LocalDate startDate) {
-        if (startDate != null) {
-            query += " AND cover_from >= '" + startDate + "'";
+    PolicyFilterQueryBuilder policyStartDateFrom(LocalDate startDateFrom) {
+        if (startDateFrom != null) {
+            mapSqlParameterSource.addValue("startDateFrom", startDateFrom);
+            query += " AND cover_from >= :startDateFrom";
         }
         return this;
     }
 
-    PolicyFilterQueryBuilder policyStartDateTo(LocalDate startDate) {
-        if (startDate != null) {
-            query += " AND cover_from <= '" + startDate + "'";
+    PolicyFilterQueryBuilder policyStartDateTo(LocalDate startDateTo) {
+        if (startDateTo != null) {
+            mapSqlParameterSource.addValue("startDateTo", startDateTo);
+            query += " AND cover_from <= :startDateTo";
         }
         return this;
     }
 
     PolicyFilterQueryBuilder policyCarPlateNumberLike(String carPlateNumber) {
-        if (!StringUtils.isEmpty(carPlateNumber)) {
-            query += " AND vehicle like '%" + carPlateNumber + "%'";
+        if (isNotEmpty(carPlateNumber)) {
+            mapSqlParameterSource.addValue("carPlateNumber", getValueForLike(carPlateNumber));
+            query += " AND vehicle like :carPlateNumber";
         }
         return this;
     }
 
-    List<PolicyInfoDto> execute(JdbcTemplate jdbcTemplate, PolicyInfoDtoMapRow policyInfoDtoMapRow) {
-        return jdbcTemplate.query(query, policyInfoDtoMapRow);
+    List<PolicyInfoDto> execute(NamedParameterJdbcTemplate jdbcTemplate, PolicyInfoDtoMapRow policyInfoDtoMapRow) {
+        return jdbcTemplate.query(query, mapSqlParameterSource, policyInfoDtoMapRow);
     }
+
+    private String getValueForLike(String policyHolder) {
+        return PERCENTAGE_SYMBOL + policyHolder + PERCENTAGE_SYMBOL;
+    }
+
+    private boolean isNotEmpty(String value) {
+        return !StringUtils.isEmpty(value);
+    }
+
 }
--- separatemodels/src/main/java/pl/altkom/asc/lab/cqrs/intro/separatemodels/readmodel/PolicyInfoDtoFinder.java
+++ separatemodels/src/main/java/pl/altkom/asc/lab/cqrs/intro/separatemodels/readmodel/PolicyInfoDtoFinder.java
@@ -2,6 +2,7 @@ package pl.altkom.asc.lab.cqrs.intro.separatemodels.readmodel;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,7 +11,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PolicyInfoDtoFinder {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
     public List<PolicyInfoDto> findByFilter(PolicyFilter filter) {
         PolicyFilterQueryBuilder policyQueryBuilder = new PolicyFilterQueryBuilder();
